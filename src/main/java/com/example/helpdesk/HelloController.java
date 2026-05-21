c5a6f16ee5aac4d656cdd60abf441f6ae666fdab
drago <dragoposh1@gmail.com>
2026-05-21 11:24:54 +0300

    Module 2 без доработки

---
+++ src/main/java/com/example/helpdesk/HelloController.java
@@ -0,0 +1,21 @@
+//package com.example.helpdesk;
+//
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//
+//public class HelloController {
+//    @GetMapping("/")
+//    public String hello() {
+//        return "ПРивед недруг!";
+//    }
+//    @GetMapping("/status")
+//    public String status() {
+//        return "СТраница (не приложение) загружена, ЧЕЕЕЕЕЕЕЕЛ!!!!!";
+//    }
+//    @GetMapping("/about")
+//    public String about() {
+//        return "ХелпДЕск проектирован ПОШ ДМИТРИЕМ ЕВГЕНЬЕВИЧЕМ !!!!!!!!!!!!!";
+//    }
+//}
